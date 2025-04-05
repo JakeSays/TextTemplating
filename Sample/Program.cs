046ada5620dace1d673328cb3feffb8ba1d8fefe
Jake Helfert <jake@helfert.us>
2025-04-05 08:40:35 -0600

    Finished sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,2 +1,11 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using T4Happiness;
+
+
+var template = new SimpleTemplate();
+//The parameters that were specified in the template file are generated as properties on the template class.
+template.SomeText = "some text";
+var text = template.TransformText();
+Console.WriteLine(text);
+
+//results in "Hello World from 'some text'!" being written to the console
